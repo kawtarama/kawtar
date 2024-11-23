9381ef2e0a771f180aa76c4917ca967289aef747
kawtarama <140628656+kawtarama@users.noreply.github.com>
2024-11-23 17:12:28 +0100

    Update loop.js

--- loop.js
+++ loop.js
@@ -1,14 +1,1 @@
-for (let nomber = 1;nomber<=30;nomber++){
-    if (nomber % 2 !== 0){
 
-    console.log(nomber)
-
- }
-}
-
- const tab = ["banana","fraise","bo"]
- for (k=0;k<tab.length;k++){
-    
-    console.log(tab[k].toUpperCase())
-
- }
